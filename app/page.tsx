d21f4f275559dccd8bd0d299443dc39a0304da8c
zetsu19 <zelmeganbayr0408@gmail.com>
2026-03-22 21:57:02 +0800

    bruhh

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function ClassMembersPage() {
         onClick={() => router.push("/admin/classMemberAdd")}
         className="bg-black text-white px-4 py-2 rounded"
       >
-        Go to Class Page
+        Go to Class Member Create
       </button>
     </div>
   );
